ac5d019dd18ac237af381d87abfa5bef9e20a469
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-23 10:24:20 +0100

    Setup of C.I & PWA
    Setting up continuous integration and Progressive Web App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { HttpClientModule } from "@angular/common/http";
 import { CurrencyDetailsComponent } from './components/currency-details/currency-details.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NavBarComponent, DashboardComponent, CurrencyDetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
